50a0d1817fc2a34fc3e27034eae887f4f238508f
JMBarth <juniior.barth@gmail.com>
2017-08-08 21:19:48 -0300

    Commit Inicial

---
+++ src/main/java/com/despensa_inteligente/DespensaInteligenteApplication.java
@@ -0,0 +1,12 @@
+package com.despensa_inteligente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DespensaInteligenteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DespensaInteligenteApplication.class, args);
+	}
+}
