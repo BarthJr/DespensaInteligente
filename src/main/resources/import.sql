e38cb5a22f81b7ef4f6cc7a604e09a31e2f6ed70
JMBarth <juniior.barth@gmail.com>
2017-09-11 09:39:40 -0300

    Adicionado dados base de cliente, produto e despensa

---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+insert into cliente(id, nome, login, senha) values(1,'Lacuna Na Mata', 'abc', '123');
+insert into produto(id, marca, nome, peso, tipo, cliente_id) values(1, 'Renata', 'Macarrão', 1, 'Semola com ovos', 1);
+insert into despensa(id, localizacao, nome) values(1, 'Curitiba', 'Casa');
