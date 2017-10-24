178a6dd766b12fb26f9a11d36fcaca3a6fb0b046
Unknown <juniior.barth@gmail.com>
2017-10-24 20:37:18 -0200

    Adicionada busca de despensa por um determinado cliente

--- src/main/java/com/despensa_inteligente/controller/DespensaController.java
+++ src/main/java/com/despensa_inteligente/controller/DespensaController.java
@@ -18,7 +18,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.despensa_inteligente.model.Cliente;
 import com.despensa_inteligente.model.Despensa;
+import com.despensa_inteligente.repository.ClienteRepository;
 import com.despensa_inteligente.repository.DespensaRepository;
 
 @SuppressWarnings("serial")
@@ -27,6 +29,7 @@ import com.despensa_inteligente.repository.DespensaRepository;
 public class DespensaController implements Serializable {
 	
 	@Autowired private DespensaRepository repository;
+	@Autowired private ClienteRepository clienteRepository;
 	     
     @GetMapping("/{id}")
 	public ResponseEntity<Despensa> buscar(@PathVariable("id") Long id) {
@@ -44,6 +47,16 @@ public class DespensaController implements Serializable {
 		}
         return new ResponseEntity<List<Despensa>>(despensa,HttpStatus.OK);
     }
+    
+    @GetMapping(value="/cliente/{id}")
+	public ResponseEntity<List<Despensa>> buscaDespensaPorCliente(@PathVariable("id") Long id) {
+    	Cliente cliente = clienteRepository.findOne(id);
+    	List<Despensa> despensa = (List<Despensa>) repository.findByCliente(cliente);
+  	if(despensa == null) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+      return new ResponseEntity<List<Despensa>>(despensa,HttpStatus.OK);
+	}
     
     @PostMapping
     public ResponseEntity<?> save(@RequestBody Despensa despensa) {
--- src/main/java/com/despensa_inteligente/repository/DespensaRepository.java
+++ src/main/java/com/despensa_inteligente/repository/DespensaRepository.java
@@ -6,10 +6,12 @@ import java.util.Optional;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import com.despensa_inteligente.model.Cliente;
 import com.despensa_inteligente.model.Despensa;
 
 @RepositoryRestResource
 public interface DespensaRepository extends CrudRepository<Despensa, Long> {
 	List<Despensa> findByNomeIgnoreCaseContaining(Optional<String> nome);
+	List<Despensa> findByCliente(Cliente cliente);
 
 }
