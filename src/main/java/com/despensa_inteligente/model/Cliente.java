3d6861140da4938be9fe23f4047fa057a06e6894
Unknown <juniior.barth@gmail.com>
2017-10-24 17:08:40 -0200

    Adicionado Construtor de Cliente

--- src/main/java/com/despensa_inteligente/model/Cliente.java
+++ src/main/java/com/despensa_inteligente/model/Cliente.java
@@ -1,17 +1,38 @@
 package com.despensa_inteligente.model;
 
-import javax.persistence.Entity;
+import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 @SuppressWarnings("serial")
 @Entity
 @Data
 @EqualsAndHashCode(callSuper = 	false)
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
 public class Cliente extends AbstractModel {
 
 	private String nome;
 	private String login;
-	public String senha;
+	private String senha;
+	
+	@ManyToMany
+	@JoinTable(name = "Favorita",   
+	joinColumns = @JoinColumn(name = "cliente_id", referencedColumnName = "id"),   
+	inverseJoinColumns = @JoinColumn(name = "receita_id", referencedColumnName = "id"))
+//	@JsonIgnoreProperties("FavoritadasPelosClientes")
+//	@JsonIgnore
+	private List<Receita> receitasFavoritas;
 }
