e7d2c419f87c82d2c5bbce1a12a062c713c3e29d
Unknown <juniior.barth@gmail.com>
2017-10-24 17:08:13 -0200

    Adicionado construtor de Categoria

--- src/main/java/com/despensa_inteligente/model/Categoria.java
+++ src/main/java/com/despensa_inteligente/model/Categoria.java
@@ -4,13 +4,17 @@ import javax.persistence.Entity;
 
 import org.hibernate.validator.constraints.NotEmpty;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 @SuppressWarnings("serial")
 @Entity
 @Data
 @EqualsAndHashCode(callSuper = false)
+@NoArgsConstructor
+@AllArgsConstructor
 public class Categoria extends AbstractModel {
 	@NotEmpty(message = "O campo nome da categoria é obrigatório")
 	private String nome;
