252346dee9c553b3fa502d7c27113e9063dc1848
Unknown <juniior.barth@gmail.com>
2017-10-24 17:09:12 -0200

    Adicionado Construtor de Medida

--- src/main/java/com/despensa_inteligente/model/Medida.java
+++ src/main/java/com/despensa_inteligente/model/Medida.java
@@ -2,13 +2,17 @@ package com.despensa_inteligente.model;
 
 import javax.persistence.Entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 @SuppressWarnings("serial")
 @Entity
 @Data
 @EqualsAndHashCode(callSuper = false)
+@NoArgsConstructor
+@AllArgsConstructor
 public class Medida extends AbstractModel {
 
 	private String nome;
