a3f8e1feaedbc332eca896ae31b8f413d94e9932
Unknown <juniior.barth@gmail.com>
2017-10-24 17:08:56 -0200

    Adicionado construtor de Despensa

--- src/main/java/com/despensa_inteligente/model/Despensa.java
+++ src/main/java/com/despensa_inteligente/model/Despensa.java
@@ -1,40 +1,44 @@
 package com.despensa_inteligente.model;
 
-import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
-import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 @SuppressWarnings("serial")
 @Entity
 @Data
 @EqualsAndHashCode(callSuper = false)
+@NoArgsConstructor
+@AllArgsConstructor
 //@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
 public class Despensa extends AbstractModel {
 
 	private String nome;
 	private String localizacao;
+
+//	@OneToMany(mappedBy = "despensa", cascade = CascadeType.ALL, orphanRemoval = false,
+//			   fetch = FetchType.LAZY)
+	@OneToMany(mappedBy = "despensa")
+	@JsonIgnoreProperties("despensa")
+//	@JsonBackReference
+	private List<ProdutoDespensa> produtosDespensas;
 	
-	@ManyToMany(mappedBy="despensas", cascade = {CascadeType.MERGE})
-//	@JoinTable(name = "ProdutoDespensa",   
-//	joinColumns = @JoinColumn(name = "despensa_id", referencedColumnName = "id"),   
-//	inverseJoinColumns = @JoinColumn(name = "produto_id", referencedColumnName = "id")) 
-	@JsonIgnoreProperties("despensas")
-	private List<Produto> produtos  = new ArrayList<>();; 
-//	private Set<Produto> produtos = new HashSet<>();
-//	private Set<Produto> produtos;
+	@ManyToOne
+	private Cliente cliente;
+	
+	public Despensa(String nome, String localizacao) {
+		super();
+		this.nome = nome;
+		this.localizacao = localizacao;
+	}
 	
 }
