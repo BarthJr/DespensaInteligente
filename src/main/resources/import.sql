73186c3ead6c6954d3c50c823b8a4198eb458723
Unknown <juniior.barth@gmail.com>
2017-10-26 20:57:01 -0200

    Adicionado campo validade na tabela produto_despensa

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -6,7 +6,7 @@ insert into produto(id, marca, nome, peso, tipo, categoria_id, cliente_id) value
 insert into produto(id, marca, nome, peso, tipo, categoria_id, cliente_id) values(2, 'Nestle', 'Chocolate', 2, 'Ao leite', 2, 1);
 insert into despensa(id, localizacao, nome, cliente_id) values(1, 'Curitiba', 'Casa', 2);
 insert into despensa(id, localizacao, nome, cliente_id) values(2, 'Floripa', 'Casa praia', 1);
-insert into produto_despensa(id, quantidade, despensa_id, produto_id) values(1, 5.0, 1, 1);
+insert into produto_despensa(id, quantidade, validade, despensa_id, produto_id) values(1, 5.0, now(), 1, 1);
 INSERT INTO receita(id, modo_preparo, quantidade, titulo, cliente_id) VALUES (1, 'Mexer', 2.0, 'Algo Legal', 1);
 INSERT INTO medida(id, nome, valor) VALUES (1, 'gramas', 2.0);
 INSERT INTO produto_receita(id, quantidade, medida_id, produto_id, receita_id) VALUES (1, 55.0, 1, 1, 1);
