12104efa9d423d443b18e1b2d84590da54475dc5
Unknown <juniior.barth@gmail.com>
2017-10-26 20:53:48 -0200

    Adicionada notação para funcionar de forma correta o LocalDate

--- src/main/java/com/despensa_inteligente/DespensaInteligenteApplication.java
+++ src/main/java/com/despensa_inteligente/DespensaInteligenteApplication.java
@@ -2,7 +2,12 @@ package com.despensa_inteligente;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 
+import com.despensa_inteligente.model.ProdutoDespensa;
+
+@EntityScan(basePackageClasses = {DespensaInteligenteApplication.class, Jsr310JpaConverters.class})
 @SpringBootApplication
 public class DespensaInteligenteApplication {
 
