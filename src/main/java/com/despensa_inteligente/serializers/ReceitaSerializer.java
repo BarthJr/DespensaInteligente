41c39f92871794efdfc4e56a3dec97775782713a
Unknown <juniior.barth@gmail.com>
2017-11-06 12:04:07 -0200

    Refatoração ReceitaSerializer

--- src/main/java/com/despensa_inteligente/serializers/ReceitaSerializer.java
+++ src/main/java/com/despensa_inteligente/serializers/ReceitaSerializer.java
@@ -1,37 +1,29 @@
 package com.despensa_inteligente.serializers;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
-import com.despensa_inteligente.model.Receita;
+import com.despensa_inteligente.model.Produto;
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.SerializerProvider;
 import com.fasterxml.jackson.databind.ser.std.StdSerializer;
 
-public class ReceitaSerializer extends StdSerializer<Receita> {
-	public ReceitaSerializer() {
+public class ProdutoSerializer extends StdSerializer<Produto> {
+	public ProdutoSerializer() {
         this(null);
     }
  
-    public ReceitaSerializer(Class<Receita> t) {
+    public ProdutoSerializer(Class<Produto> t) {
         super(t);
     }
  
     @Override
     public void serialize(
-      Receita receita, 
+      Produto produto, 
       JsonGenerator generator, 
       SerializerProvider provider) 
       throws IOException, JsonProcessingException {
-         
-    	generator.writeStartObject();
-        generator.writeNumberField("id", receita.getId());
-        generator.writeStringField("titulo", receita.getTitulo());
-        generator.writeStringField("modoPreparo", receita.getModoPreparo());
-        generator.writeNumberField("cliente", receita.getCliente().getId());
-        generator.writeEndObject();
+        generator.writeNumber(produto.getId());
     }
 
 }
