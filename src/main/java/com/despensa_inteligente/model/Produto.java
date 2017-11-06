61cd7b84d2a3b28e5e92328c1e0544b84b1f238d
Unknown <juniior.barth@gmail.com>
2017-11-06 10:24:18 -0200

    Refatoração Produto

--- src/main/java/com/despensa_inteligente/model/Produto.java
+++ src/main/java/com/despensa_inteligente/model/Produto.java
@@ -6,10 +6,10 @@ import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 
-import com.despensa_inteligente.serializers.ProdutoSerializer;
+import com.despensa_inteligente.serializers.*;
+import com.fasterxml.jackson.annotation.*;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
@@ -20,35 +20,60 @@ import lombok.ToString;
 @Data
 @EqualsAndHashCode(callSuper = false)
 @NoArgsConstructor
-@AllArgsConstructor
 @ToString
-@JsonSerialize(using = ProdutoSerializer.class)
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Produto extends AbstractModel {
 
 	private String nome;
 	private String marca;
 	private String tipo;
-	private Double peso;
-	
-	
-	public Produto(String nome, String marca, String tipo, Double peso) {
-		super();
-		this.nome = nome;
-		this.marca = marca;
-		this.tipo = tipo;
-		this.peso = peso;
-	}
+    private Double peso;
 
 	@ManyToOne
+    @JsonIgnore
 	private Cliente cliente;
-	
+
 	@ManyToOne
+    @JsonSerialize(using = CategoriaSerializer.class)
 	private Categoria categoria;
-	
+
 	@OneToMany(mappedBy = "produto")
+    @JsonIgnore
 	private List<ProdutoDespensa> produtosDespensas;
-	
+
 	@OneToMany(mappedBy = "produto")
+    @JsonIgnore
 	private List<ProdutoReceita> produtosReceitas;
-	
+
+	//Constructor GET
+    public Produto(Long id, String nome, String marca, String tipo,
+                   Double peso, Long idCategoria) {
+        super(id);
+        Categoria categoria = new Categoria(idCategoria);
+
+        this.nome = nome;
+        this.marca = marca;
+        this.tipo = tipo;
+        this.peso = peso;
+        this.categoria = categoria;
+    }
+
+    // Constructor POST
+    public Produto(String nome, String marca, String tipo,
+                   Double peso, Long idCategoria) {
+        super();
+        Categoria categoria = new Categoria(idCategoria);
+
+        this.nome = nome;
+        this.marca = marca;
+        this.tipo = tipo;
+        this.peso = peso;
+        this.categoria = categoria;
+    }
+
+
+    public Produto(Long id){
+        super(id);
+    }
+
 }
