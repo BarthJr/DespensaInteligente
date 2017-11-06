121759aa92c35a931944e229dcdb25478d8c305f
Unknown <juniior.barth@gmail.com>
2017-11-06 12:25:37 -0200

    Add novas consultas

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,6 @@
 insert into categoria(id, nome) values(1, 'Congelado');
 insert into categoria(id, nome) values(2, 'Fruta');
+insert into categoria(id,nome) values(3, 'Para Deletar');
 insert into cliente(id, nome, login, senha) values(1,'Lacuna Na Mata', 'abc', '123');
 insert into cliente(id, nome, login, senha) values(2,'Guido van Rossum', 'abcd', '1234');
 insert into produto(id, marca, nome, peso, tipo, categoria_id, cliente_id) values(1, 'Renata', 'Macarrão', 1, 'Semola com ovos', 1, 2);
@@ -7,8 +8,11 @@ insert into produto(id, marca, nome, peso, tipo, categoria_id, cliente_id) value
 insert into despensa(id, localizacao, nome, cliente_id) values(1, 'Curitiba', 'Casa', 2);
 insert into despensa(id, localizacao, nome, cliente_id) values(2, 'Floripa', 'Casa praia', 1);
 insert into produto_despensa(id, quantidade, validade, despensa_id, produto_id) values(1, 5.0, now(), 1, 1);
+insert into produto_despensa(id, quantidade, validade, despensa_id, produto_id) values(2, 10.0, now(), 2, 2);
+insert into produto_despensa(id, quantidade, validade, despensa_id, produto_id) values(3, 15.0, now(), 1, 1);
 INSERT INTO receita(id, modo_preparo, quantidade, titulo, cliente_id) VALUES (1, 'Mexer', 2.0, 'Algo Legal', 1);
 INSERT INTO medida(id, nome, valor) VALUES (1, 'gramas', 2.0);
+INSERT INTO medida(id, nome, valor) VALUES (2, 'test', 5.0);
 INSERT INTO produto_receita(id, quantidade, medida_id, produto_id, receita_id) VALUES (1, 55.0, 1, 1, 1);
 
 INSERT INTO receita(id, modo_preparo, quantidade, titulo, cliente_id) VALUES (2, 'Colocar em agua fervente por 3 minutos', 1.0, 'Miojo', 2);
